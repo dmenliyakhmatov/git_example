dfa2db53bf53570805823409b33cd9d7add32462
Dmitriy <dmenl@mail.ru>
2024-01-27 12:39:29 +0300

    add comment to main

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-import 'normalize.css';
-import { App } from './app';
+import "normalize.css";
+import { App } from "./app";
 
-const app = document.getElementById('app');
-
+const app = document.getElementById("app");
+///comment
 if (app) {
   const root = createRoot(app);
 
