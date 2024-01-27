85fe4d855df0640c1fba7b2038291c3662480300
Dmitriy <dmenl@mail.ru>
2024-01-27 12:36:43 +0300

    add changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
 export const App = () => {
   return <RouterProvider router={router} />;
 };
+console.log("!!!");
